45682419aded3e8801534aca0e6e4ccc87293db3
markman700 <markman700@gmail.com>
2025-07-22 22:40:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // 1. Initialize Quill
-// const quill = new quill('#editor', {
-const quill('#editor', {
+const quill = new quill('#editor', {
   theme: 'snow',
   placeholder: 'Start writing your sermon...',
   modules: {
